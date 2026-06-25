ec84a7431c7ae2660aefed35e15e3d7a22a21b31
guadafestinese96 <guadalupefestinese@gmail.com>
2026-06-24 22:30:42 -0300

    Agrego datos

--- js/login.js
+++ js/login.js
@@ -1,10 +1,10 @@
 document.getElementById("login-form").addEventListener("submit", function (e) {
   e.preventDefault();
-  const user = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
   const pass = document.getElementById("password").value;
 
   //validacion harcodeada para probar
-  if (user === "admin" && pass === "1234") {
+  if (email === "admin@admin.com" && pass === "1234") {
     window.location.href = "dashboard.html";
   } else {
     alert("Usuario o contraseña incorrectos");
